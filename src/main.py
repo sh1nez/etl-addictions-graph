7a0c0d04b9ed20a00dfcd680a0192b8880de469a
Taroso0 <kuznetsovalexev@gmail.com>
2025-04-09 14:07:45 +0300

    fix: visual
    * "refactor: change visual of circles"
    
    * "refactor: do much better with visual"

--- src/main.py
+++ src/main.py
@@ -62,13 +62,8 @@ class GraphVisualizer:
         G.add_edges_from(storage.edges)
         plt.figure(figsize=(10, 6))
         try:
-            pos = nx.spring_layout(G)
-            # * Есть разные методы layout, в теории потом можно будет использовать nx.multipartite_layout(),
-            # * который послойно будет отображать ноды, т.е. можно отображать что-то наподобии этапов
-            # * pos = nx.multipartite_layout(G, subset_key="layer")
-            # * или можно использовать тот же spring_layout, но с параметрами для большего расстояния между нодами:
-            # * n = G.number_of_nodes()
-            # * nx.spring_layout(G, k=1 / (n**0.0625), iterations=100, scale=2)
+            pos = nx.spring_layout(G, k=1.2, iterations=100, scale=3, seed=42)
+
             colors = nx.get_edge_attributes(G, "color").values()
             labels = nx.get_edge_attributes(G, "operation")
             nx.draw(
@@ -77,8 +72,8 @@ class GraphVisualizer:
                 with_labels=True,
                 node_color="lightblue",
                 edge_color=colors,
-                font_size=10,
-                node_size=2000,
+                font_size=9,  # было 10
+                node_size=2200,  # было 2000
             )
             nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
             if title:
