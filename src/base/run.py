2951d6c5aef0a2ad758f1eac6d1764019c27a6bd
Дмитрий <145381480+TAskMAster339@users.noreply.github.com>
2025-04-14 11:28:38 +0300

    test: dialects (#44)
    also fix lib version and black formatting

--- src/base/run.py
+++ src/base/run.py
@@ -38,8 +38,7 @@ def main():
                 temp_storage.add_dependencies(dependencies)
                 manager.visualizer.render(
                     temp_storage,
-                    f"Dependencies for {
-                        os.path.basename(file_path)}",
+                    f"Dependencies for {os.path.basename(file_path)}",
                 )
         else:
             results = manager.process_directory(directory)
