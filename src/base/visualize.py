d71600d85b34fd7ed60184e9df5222807834307e
HROMAYAKOZA <124992597+HROMAYAKOZA@users.noreply.github.com>
2025-04-26 00:03:53 +0300

    feat: multiedge-visualisation (#63)
    Co-authored-by: 2happy <biv888iii888@yandex.ru>

--- src/base/visualize.py
+++ src/base/visualize.py
@@ -11,7 +11,7 @@ class GraphVisualizer:
         if not storage.nodes:
             print("Graph is empty, no dependencies to display.")
             return
-        G = nx.DiGraph()
+        G = nx.MultiDiGraph()
         G.add_nodes_from(storage.nodes)
         G.add_edges_from(storage.edges)
         plt.figure(figsize=(12, 8))
@@ -22,11 +22,18 @@ class GraphVisualizer:
             edge_colors = [data["color"] for u, v, data in G.edges(data=True)]
 
             # Подготовка меток для рёбер
-            edge_labels = {}
-            for u, v, data in G.edges(data=True):
-                # Берем только имя операции без деталей
-                label = data.get("operation", "")
-                edge_labels[(u, v)] = label
+            edge_labels = {
+                (u, v, k): d["operation"]
+                for u, v, k, d in G.edges(keys=True, data=True)
+            }
+
+            # насколько одна связь отдаляется от другой
+            step = 0.1
+            # максимальное количество отображаемых связей между двумя нодами
+            multi_edge_lim = 10
+            connectionstyle = [
+                f"arc3,rad={(-1)**(i+1)*((i+1)//2*step)}" for i in range(multi_edge_lim)
+            ]
 
             # Отрисовка графа
             nx.draw(
@@ -39,8 +46,15 @@ class GraphVisualizer:
                 node_size=2000,
                 arrows=True,
                 arrowsize=15,
+                connectionstyle=connectionstyle,
             )
-            nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=9)
+            nx.draw_networkx_edge_labels(
+                G,
+                pos,
+                edge_labels=edge_labels,
+                font_size=9,
+                connectionstyle=connectionstyle,
+            )
 
             if title:
                 plt.title(title)
